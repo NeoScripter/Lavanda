7230d7a602e5285f616d80e1c77d8328fbac8dcc
Neoscripter <sange0337@gmail.com>
2025-12-15 19:36:24 +0800

    add decor images to the plans section

--- resources/js/pages/user/Home/partials/PlanSection/PlanSection.tsx
+++ resources/js/pages/user/Home/partials/PlanSection/PlanSection.tsx
@@ -1,7 +1,15 @@
+import LazyImage from '@/components/user/ui/LazyImage/LazyImage';
 import PlanCard from '@/components/user/ui/PlanCard/PlanCard';
 import { Plan } from '@/types/model';
 import { usePage } from '@inertiajs/react';
 import css from './PlanSection.module.scss';
+import Crystal from '@/assets/images/home/plans-decor-1.webp';
+import CrystalTiny from '@/assets/images/home/plans-decor-1-tiny.webp';
+import Butterfly from '@/assets/images/home/plans-decor-2.webp';
+import ButterflyTiny from '@/assets/images/home/plans-decor-2-tiny.webp';
+import Stone from '@/assets/images/home/hero-decor.webp';
+import StoneTiny from '@/assets/images/home/hero-decor-tiny.webp';
+
 
 const PlanSection = () => {
     const { plans } = usePage<{ plans: Plan[] }>().props;
@@ -27,6 +35,22 @@ const PlanSection = () => {
                     />
                 ))}
             </ul>
+
+            <LazyImage
+                img={Butterfly}
+                tinyImg={ButterflyTiny}
+                prtClass={css.decorPlans1}
+            />
+            <LazyImage
+                img={Crystal}
+                tinyImg={CrystalTiny}
+                prtClass={css.decorPlans2}
+            />
+            <LazyImage
+                img={Stone}
+                tinyImg={StoneTiny}
+                prtClass={css.decorPlans3}
+            />
         </section>
     );
 };
