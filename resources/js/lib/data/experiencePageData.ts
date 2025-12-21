a92c31b961d7e17051e3af3844e29bdc9068bc71
Neoscripter <sange0337@gmail.com>
2025-12-21 16:41:43 +0800

    insert bg images into experience item cards

---
+++ resources/js/lib/data/experiencePageData.ts
@@ -0,0 +1,58 @@
+import Item1 from '@/assets/images/experience/experience-item-1.webp';
+import Item1Tiny from '@/assets/images/experience/experience-item-1-tiny.webp';
+import Item2 from '@/assets/images/experience/experience-item-2.webp';
+import Item2Tiny from '@/assets/images/experience/experience-item-2-tiny.webp';
+import Item3 from '@/assets/images/experience/experience-item-3.webp';
+import Item3Tiny from '@/assets/images/experience/experience-item-3-tiny.webp';
+import Item4 from '@/assets/images/experience/experience-item-4.webp';
+import Item4Tiny from '@/assets/images/experience/experience-item-4-tiny.webp';
+import Item5 from '@/assets/images/experience/experience-item-5.webp';
+import Item5Tiny from '@/assets/images/experience/experience-item-5-tiny.webp';
+import Item6 from '@/assets/images/experience/experience-item-6.webp';
+import Item6Tiny from '@/assets/images/experience/experience-item-6-tiny.webp';
+
+export type ImageObj = {
+    id: string;
+    img: string;
+    tinyImg: string;
+    alt: string;
+}
+
+export const images: ImageObj[] = [
+    {
+        id: crypto.randomUUID(),
+        img: Item1,
+        tinyImg: Item1Tiny,
+        alt: 'Белые бумажные или пластиковые веерообразные элементы разной высоты',
+    },
+    {
+        id: crypto.randomUUID(),
+        img: Item2,
+        tinyImg: Item2Tiny,
+        alt: 'Абстрактная белая композиция из гладких изогнутых форм',
+    },
+    {
+        id: crypto.randomUUID(),
+        img: Item3,
+        tinyImg: Item3Tiny,
+        alt: 'Минималистичные белые изогнутые линии на светлом фоне',
+    },
+    {
+        id: crypto.randomUUID(),
+        img: Item4,
+        tinyImg: Item4Tiny,
+        alt: 'Нежные белые складки шелковистой ткани с плавными изгибами',
+    },
+    {
+        id: crypto.randomUUID(),
+        img: Item5,
+        tinyImg: Item5Tiny,
+        alt: 'Текстурированная белая ткань с естественными складками и переплетениями волокон',
+    },
+    {
+        id: crypto.randomUUID(),
+        img: Item6,
+        tinyImg: Item6Tiny,
+        alt: 'Абстрактные белые волнистые складки ткани с мягкими тенями',
+    },
+];
--- resources/js/pages/user/Decision/pages/Experience/partials/ItemSection/ItemSection.tsx
+++ resources/js/pages/user/Decision/pages/Experience/partials/ItemSection/ItemSection.tsx
@@ -1,21 +1,46 @@
+import LazyImage from '@/components/user/ui/LazyImage/LazyImage';
+import { ImageObj, images } from '@/lib/data/experiencePageData';
 import { ExperienceItem } from '@/types/model';
 import { NodeProps } from '@/types/nodeProps';
 import { cn } from '@/utils/cn';
 import { usePage } from '@inertiajs/react';
-import { FC } from 'preact/compat';
+import { FC, useCallback, useRef } from 'preact/compat';
 import css from './ItemSection.module.scss';
 
 const ItemSection: FC<NodeProps> = ({ className }) => {
+    const prevIdx = useRef<number | null>(null);
+
+    const getRandomImage = useCallback(
+        (prev: number | null, length: number) => {
+            let idx = Math.floor(Math.random() * length);
+
+            while (idx === prev) {
+                idx = Math.floor(Math.random() * length);
+            }
+
+            return idx;
+        },
+        [length],
+    );
+
     const { items } = usePage<{ items: ExperienceItem[] }>().props;
     return (
         <section class={cn(css.wrapper, className)}>
             <ul class={css.items}>
-                {items.map((item) => (
-                    <ItemCard
-                        key={item.id}
-                        item={item}
-                    />
-                ))}
+                {items.map((item) => {
+                    prevIdx.current = getRandomImage(
+                        prevIdx.current,
+                        images.length,
+                    );
+
+                    return (
+                        <ItemCard
+                            img={images[prevIdx.current]}
+                            key={item.id}
+                            item={item}
+                        />
+                    );
+                })}
             </ul>
         </section>
     );
@@ -23,9 +48,18 @@ const ItemSection: FC<NodeProps> = ({ className }) => {
 
 export default ItemSection;
 
-const ItemCard: FC<{ item: ExperienceItem }> = ({ item }) => {
+const ItemCard: FC<{ item: ExperienceItem; img: ImageObj }> = ({
+    item,
+    img,
+}) => {
     return (
         <li class={css.card}>
+            <LazyImage
+                prtClass={css.cardOverlay}
+                img={img.img}
+                tinyImg={img.tinyImg}
+                alt={img.alt}
+            />
             <h2 class={css.cardHeading}>{item.title}</h2>
             <p class={css.cardDescription}>{item.description}</p>
         </li>
