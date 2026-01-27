ddf9ad4196d32797a2c2b2b198c4e426369d8113
Neoscripter <sange0337@gmail.com>
2026-01-27 12:26:55 +0800

    Remove animation from faqs and revert it to default browser behavior

--- resources/js/pages/user/Home/partials/FAQSection/FAQSection.tsx
+++ resources/js/pages/user/Home/partials/FAQSection/FAQSection.tsx
@@ -1,42 +1,19 @@
 import { faqItems } from '@/lib/data/faqItems';
 import { cn } from '@/utils/cn';
-import { useRef, useState } from 'preact/hooks';
 import css from './FAQSection.module.scss';
 
 const FAQSection = () => {
-    const [openId, setOpenId] = useState<number | null>(null);
-    const detailsRefs = useRef<{ [key: number]: HTMLDetailsElement }>({});
-
-    const toggle = (id: number) => {
-        const isOpening = openId !== id;
-        setOpenId(isOpening ? id : null);
-
-        if (detailsRefs.current[id]) {
-            detailsRefs.current[id].open = isOpening;
-        }
-    };
-
     return (
-        <section class={css.faqWrapper}>
+        <section id="faqs" class={css.faqWrapper}>
             <h2 className={css.heading}>Вопросы и ответы</h2>
 
             <div className={css.faqs}>
                 {faqItems.map((faq) => (
                     <details
                         key={faq.id}
-                        open={openId === faq.id}
-                        ref={(el) => {
-                            if (el) detailsRefs.current[faq.id] = el;
-                        }}
                         className={css.details}
                     >
-                        <summary
-                            onClick={(e) => {
-                                e.preventDefault();
-                                toggle(faq.id);
-                            }}
-                            className={css.summary}
-                        >
+                        <summary className={css.summary}>
                             {faq.question}
                             <span className={css.icon}>
                                 <svg
@@ -49,12 +26,7 @@ const FAQSection = () => {
                                         stroke="#111118"
                                         stroke-width="1.77778"
                                         stroke-linecap="round"
-                                        class={cn(
-                                            css.path,
-                                            openId === faq.id
-                                                ? css.rotated
-                                                : css.static,
-                                        )}
+                                        class={cn(css.path)}
                                     />
                                 </svg>
                             </span>
