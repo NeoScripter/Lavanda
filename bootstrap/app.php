0629786596224691198a0da21d63aa6323e4ed4b
Neoscripter <sange0337@gmail.com>
2026-02-28 10:02:22 +0800

    fix:change import for Schedule in bootstrap

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,10 +1,10 @@
 <?php
 
 use App\Http\Middleware\HandleInertiaRequests;
-use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Schedule;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
