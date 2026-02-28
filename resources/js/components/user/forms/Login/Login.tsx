41b559d0aaa6b46cb40f030495feb4bf1086b64e
Neoscripter <sange0337@gmail.com>
2026-02-28 11:28:22 +0800

    debug:remove preserve state from login component

--- resources/js/components/user/forms/Login/Login.tsx
+++ resources/js/components/user/forms/Login/Login.tsx
@@ -31,7 +31,6 @@ export default function Login() {
 
         post(route('otp.send'), {
             preserveScroll: true,
-            preserveState: true,
             onSuccess: () => {
                 showOtp();
             },
