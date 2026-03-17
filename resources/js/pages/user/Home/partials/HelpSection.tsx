060d057802f69c7e51f23f88bbde3bfe14dd7b77
Neoscripter <sange0337@gmail.com>
2026-03-17 22:49:36 +0800

    fix:delete false HelpSection file from home page directory

--- resources/js/pages/user/Home/partials/HelpSection.tsx
+++
@@ -1,8 +0,0 @@
-import { NodeProps } from '@/types/nodeProps';
-import { FC } from 'preact/compat';
-
-const HelpSection: FC<NodeProps> = ({ className }) => {
-    return ()
-};
-
-export default HelpSection;
