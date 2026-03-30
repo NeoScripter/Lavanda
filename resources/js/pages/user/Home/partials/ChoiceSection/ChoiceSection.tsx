8cd4543b8d91f65030b64cd32454ed0525cbb158
Neoscripter <sange0337@gmail.com>
2026-03-30 22:12:02 +0800

    Style:display login form for unregistered users when clicked on join btn in the choice sectoin

--- resources/js/pages/user/Home/partials/ChoiceSection/ChoiceSection.tsx
+++ resources/js/pages/user/Home/partials/ChoiceSection/ChoiceSection.tsx
@@ -1,9 +1,17 @@
+import { useAuthModal } from '@/providers/AuthModalContext';
+import { Auth } from '@/types/auth';
 import { NodeProps } from '@/types/nodeProps';
 import { cn } from '@/utils/cn';
+import { usePage } from '@inertiajs/react';
 import { FC } from 'preact/compat';
 import css from './ChoiceSection.module.scss';
 
 const ChoiceSection: FC<NodeProps> = ({ children }) => {
+    const { auth } = usePage<{ auth: Auth }>().props;
+    const { showLogin } = useAuthModal();
+
+    const isLoggedIn = auth.user;
+
     return (
         <section className={cn('full-bleed', css.wrapper)}>
             <div className={css.root}>
@@ -17,16 +25,23 @@ const ChoiceSection: FC<NodeProps> = ({ children }) => {
                     привязки карты. Просто осмотритесь.
                 </p>
 
-                <div className={css.joinBtnFrame}>
-                    <button className={css.joinBtn}>
-                        Открыть доступ на 24 часа
-                    </button>
-                    <span className={css.badge}>Бесплатно</span>
-                </div>
+                {!isLoggedIn && (
+                    <>
+                        <div className={css.joinBtnFrame}>
+                            <button
+                                onClick={showLogin}
+                                className={css.joinBtn}
+                            >
+                                Открыть доступ на 24 часа
+                            </button>
+                            <span className={css.badge}>Бесплатно</span>
+                        </div>
 
-                <p className={css.highlight}>
-                    Регистрация в один клик через Google / Yandex
-                </p>
+                        <p className={css.highlight}>
+                            Регистрация в один клик через Google / Yandex
+                        </p>
+                    </>
+                )}
                 <p className={css.footnote}>
                     «Кошачий оракул» всегда открыт для вас.
                 </p>
