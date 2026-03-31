b40193ca0e767b01b8c8938ae9bc34e6bd095c71
Neoscripter <sange0337@gmail.com>
2026-03-31 18:12:02 +0800

    Fix:remove empty ListItem file

--- resources/js/pages/user/Home/partials/HelpSection/partials/ListItem.tsx
+++
@@ -1,8 +0,0 @@
-import { NodeProps } from '@/types/nodeProps';
-import { FC } from 'preact/compat';
-
-const ListItem: FC<NodeProps> = ({ className }) => {
-    return ()
-};
-
-export default ListItem;
